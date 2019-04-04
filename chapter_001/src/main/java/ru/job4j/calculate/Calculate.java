135bb7cadc257a6a46cbb4e6790cce35d90986be
alexanderbeznos <ast1bn@mail.ru>
2019-04-04 21:20:06 +0300

    1.3. JavaDoc. Code convention[#127895]

---
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -0,0 +1,15 @@
+package ru.job4j.calculate;
+
+/**
+ * Calculate.
+ 
+ * @author Alexander Beznos (ast1bn@mail.ru)
+ */
+public class Calculate {
+	/**
+	* Сложение.
+	* @param first
+	public static void main (String[] args) {
+		System.out.println("Hello World");
+	}
+}
