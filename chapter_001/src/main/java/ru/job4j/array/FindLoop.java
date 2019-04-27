c58808122c2a84e13728285bfcb5e77a96de1387
alexanderbeznos <ast1bn@mail.ru>
2019-04-27 19:12:52 +0300

    6.1. Классический поиск перебором.[#127870]

---
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -0,0 +1,27 @@
+package ru.job4j.array;
+/**
+ * Нахождение числа в массиве.
+ * @author Akexander Beznos (ast1bn@mail.ru).
+ * @version 1.0.
+ * @since 27.04.2019.
+ */
+
+public class FindLoop {
+    /**
+     * вычичление.
+     * @param date массивю
+     * @param el искомый элемент.
+     * @return возврат искомого элемента.
+     */
+
+    public int indexOf(int[] date, int el) {
+        int rst = -1;
+        for (int index = 0; index != date.length; index++) {
+            if (date[index] == el) {
+                rst = index;
+                break;
+            }
+        }
+        return rst;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
@@ -0,0 +1,50 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test
+ * @author Akexander Beznos (ast1bn@mail.ru).
+ * @version 1.0.
+ * @since 27.04.2019.
+ */
+
+public class FindLoopTest {
+    /**
+     * Test "нахождение числа 5".
+     */
+    @Test
+    public void whenArrayHas5Then0() {
+        FindLoop find = new FindLoop();
+        int[] input = {5, 10, 3};
+        int value = 5;
+        int result = find.indexOf(input, value);
+        int expect = 0;
+        assertThat(result, is(expect));
+    }
+    /**
+     * Test "нахождение числа 8".
+     */
+    @Test
+    public void whenArrayHas5Then3() {
+        FindLoop find = new FindLoop();
+        int[] input = {7, 17, 8, 14, 5};
+        int value = 8;
+        int result = find.indexOf(input, value);
+        int expect = 2;
+        assertThat(result, is(expect));
+    }
+    /**
+     * Test "нахождение числа 27".
+     */
+    @Test
+    public void whenArrayHas5Then7() {
+        FindLoop find = new FindLoop();
+        int result = find.indexOf(new int[] {5, 10, 3, 41, 27, 92, 2}, 27);
+        int expect = 4;
+        assertThat(result, is(expect));
+    }
+}
+
