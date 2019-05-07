5e19999a27020cbb96c10569ff6207d3ad1d1d90
alexanderbeznos <ast1bn@mail.ru>
2019-05-07 23:05:25 +0300

    Экзамен. Базовый синтаксис.[#127903]

--- chapter_001/src/main/java/ru/job4j/array/Merge.java
+++ chapter_001/src/main/java/ru/job4j/array/Merge.java
@@ -3,32 +3,26 @@ package ru.job4j.array;
 public class Merge {
     public int[] merge(int[] left, int[] right) {
         int[] rsl = new int[left.length + right.length];
-        int i=0, j=0;
-        for (int k=0; k<rsl.length; k++) {
-
-            if (i > left.length-1) {
+        int i = 0, j = 0;
+        for (int k = 0; k < rsl.length; k++) {
+            if (i > left.length - 1) {
                 int a = right[j];
                 rsl[k] = a;
                 j++;
-            }
-            else if (j > right.length-1) {
+            } else if (j > right.length - 1) {
                 int a = left[i];
                 rsl[k] = a;
                 i++;
-            }
-            else if (left[i] < right[j]) {
+            } else if (left[i] < right[j]) {
                 int a = left[i];
                 rsl[k] = a;
                 i++;
-            }
-            else {
+            } else {
                 int b = right[j];
                 rsl[k] = b;
                 j++;
             }
         }
-
-
         return rsl;
     }
 }
