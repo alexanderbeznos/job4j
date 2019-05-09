2db01fa3561fd63759eb8d328e245c7757855b0f
alexanderbeznos <ast1bn@mail.ru>
2019-05-09 15:48:47 +0300

    Устранение всех ошибок

--- chapter_001/src/test/java/ru/job4j/array/ArrayDuplicateTest.java
+++ chapter_001/src/test/java/ru/job4j/array/ArrayDuplicateTest.java
@@ -22,9 +22,9 @@ public class ArrayDuplicateTest {
     @Test
     public void whenRemoveDuplicatesThenArrayWithoutDuplicate() {
         ArrayDuplicate duplicate = new ArrayDuplicate();
-        String [] input = {"Привет", "Мир", "Привет", "Супер", "Мир"};
-        String [] except = {"Привет", "Мир", "Супер"};
-        String [] result = duplicate.remove(input);
+        String[] input = {"Привет", "Мир", "Привет", "Супер", "Мир"};
+        String[] except = {"Привет", "Мир", "Супер"};
+        String[] result = duplicate.remove(input);
         assertThat(result, arrayContainingInAnyOrder(except));
     }
 }
