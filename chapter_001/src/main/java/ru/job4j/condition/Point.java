584ab998ccf6d7d199f77607d1bd32964d91cf3f
alexanderbeznos <ast1bn@mail.ru>
2019-05-09 22:44:24 +0300

    1. Рефакторинг - Расстояние между точками.[#127979]

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -1,21 +1,27 @@
 package ru.job4j.condition;
 
+import static java.lang.Math.sqrt;
+import static java.lang.Math.pow;
 /**
  * Расстояния между точками в системе координат.
  */
 public class Point {
 /**
  * Нахождение расстояния.
- * @param x1 координата.
- * @param x2 координата.
- * @param y1 координата.
- * @param y2 координата.
+ * @param first координата.
+ * @param second координата.
  * @return distance.
  */
-    public double distance(int x1, int y1, int x2, int y2) {
-        double first = Math.pow(x2 - x1, 2);
-        double second = Math.pow(y2 - y1, 2);
-        double three = second +  first;
-        return Math.sqrt(three);
+    private int x;
+    private int y;
+    public Point(int first, int second) {
+        this.x = first;
+        this.y = second;
+    }
+    public double distance(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
+    }
+    public void info() {
+        System.out.println(String.format("Point[%s, %s]", this.x, this.y));
     }
 }
--- chapter_001/src/test/java/ru/job4j/condition/PointTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -7,19 +7,42 @@ import static org.junit.Assert.assertThat;
 /**
  * Test
  * @author Alexander Beznos (ast1bn@mail.ru).
- * @version 1.0.
- * since 13.04.2019;
+ * @version 1.1.
+ * since 09.05.2019;
  */
-
 public class PointTest {
     /**
-     * Test from coordinate to distance
+     * Test from coordinate to distance 0,0,0,10.
      */
     @Test
     public void whenZeroAndTenThenTen() {
-        Point point = new Point();
-        double result = point.distance(0, 0, 0, 10);
+        Point first = new Point(0, 0);
+        Point second = new Point(0, 10);
+        double result = first.distance(second);
+        first.info();
+        second.info();
+        System.out.println(String.format("Result is %s", result));
         assertThat(result, is(10d));
     }
+    /**
+     * Test from coordinate to distance 0,0.
+     */
+    @Test
+    public void whenCheckItself() {
+        Point point = new Point(0, 0);
+        double result = point.distance(point);
+        assertThat(result, is(0D));
+    }
+    /**
+     * Test from coordinate to distance 1,1,2,2.
+     */
+    @Test
+    public void whenShowInfo() {
+        Point first = new Point(1, 1);
+        first.info();
+        Point second = new Point(2, 2);
+        second.info();
+    }
+
 }
 
