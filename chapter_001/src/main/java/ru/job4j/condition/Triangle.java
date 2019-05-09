81c1c0a4a54a64f5bea7dc53480dbc71795e16c5
alexanderbeznos <ast1bn@mail.ru>
2019-05-09 22:44:56 +0300

    2. Рефакторинг - Площадь треугольника.[#127980]

--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -2,19 +2,21 @@ package ru.job4j.condition;
 
 /**
  * Вычисление площади треугольника.
- *
  * @author Akexander Beznos (ast1bn@mail.ru).
- * @version 1.0.
- * @since 14.04.2019.
+ * @version 1.1.
+ * @since 09.05.2019.
  */
-
 public class Triangle {
+    private Point first;
+    private Point second;
+    private Point third;
+    public Triangle(Point ap, Point bp, Point cp) {
+        this.first = ap;
+        this.second = bp;
+        this.third = cp;
+    }
     /**
      * Метод вычисления периметра по длинам сторон.
-     * <p>
-     * Формула.
-     * (a + b + c) / 2.
-     *
      * @param a расстояние между точками a b.
      * @param b расстояние между точками a c.
      * @param c расстояние между точками b c.
@@ -24,22 +26,15 @@ public class Triangle {
         double result = (a + b + c) / 2;
         return result;
     }
-
     /**
      * Метод должен вычислить прощадь треугольканива.
-     * <p>
-     * Формула.
-     * <p>
-     * √ p *(p - ab) * (p - ac) * (p - bc).
-     * где √ - корень квадратный, для извлечения корня.
-     *
      * @return Вернуть прощадь, если треугольник существует или -1.
      */
-    public double area(int x1, int y1, int x2, int y2, int x3, int y3) {
+    public double area() {
         double rsl = -1;
-        double a = new Point().distance(x1, y1, x2, y2);
-        double b = new Point().distance(x2, y2, x3, y3);
-        double c = new Point().distance(x1, y1, x3, y3);
+        double a = first.distance(second);
+        double b = first.distance(third);
+        double c = second.distance(third);
         double p = period(a, b, c);
         if (this.exist(a, b, c)) {
             rsl = Math.sqrt(p * (p - a) * (p - b) * (p - c));
@@ -49,13 +44,12 @@ public class Triangle {
 
     /**
      * Метод проверяет можно ли построить треугольник с такими длинами сторон.
-     *
      * @param a Длина от точки a b.
      * @param b Длина от точки a c.
      * @param c Длина от точки b c.
      * @return false.
      */
     private boolean exist(double a, double c, double b) {
-            return (a + b > c && b + c > a && a + c > b);
+        return (a + b > c && b + c > a && a + c > b);
     }
 }
