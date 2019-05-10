af94679876cccd59b2488bb36fee7a6b8f2927df
alexanderbeznos <ast1bn@mail.ru>
2019-05-10 21:41:00 +0300

    3. Расстояние между точками в трехмерном пространстве.[#127981]

--- chapter_001/src/test/java/ru/job4j/condition/PointTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -43,6 +43,16 @@ public class PointTest {
         Point second = new Point(2, 2);
         second.info();
     }
-
+    /**
+     * Test from coordinate to distance 1,0,4,0,8,0.
+     */
+    @Test
+    public void whenZeroAnd3() {
+        Point first = new Point(0, 0, 0);
+        Point second = new Point(1, 4, 8);
+        double result = first.distance3d(second);
+        System.out.println(String.format("Result is %s", result));
+        assertThat(result, is(9d));
+    }
 }
 
