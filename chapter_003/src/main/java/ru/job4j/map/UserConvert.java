eec2e00ea292bf2f24e2bb9e616d12b99ff7add3
alexanderbeznos <ast1bn@mail.ru>
2019-08-15 18:03:57 +0300

    2. Написать программу преобразования List в Map.[#127927]

---
+++ chapter_003/src/main/java/ru/job4j/map/UserConvert.java
@@ -0,0 +1,23 @@
+package main.java.ru.job4j.map;
+
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * @author Alexander Beznos (ast1bn@mail.ru).
+ * @version 1.0.
+ * @since 15.08.2019.
+ */
+public class UserConvert {
+
+    /**
+     * Метод преобразует лист в мэп
+     */
+    public HashMap<Integer, User> process(List<User> list) {
+        HashMap<Integer, User> map = new HashMap<>();
+        for (User o : list) {
+            map.put(o.getId(), o);
+        }
+        return map;
+    }
+}
---
+++ chapter_003/src/test/java/ru/job4j/collections/UserConvertTest.java
@@ -0,0 +1,24 @@
+package test.java.ru.job4j.collections;
+
+import main.java.ru.job4j.map.User;
+import main.java.ru.job4j.map.UserConvert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class UserConvertTest {
+    @Test
+    public void whenListToMap() {
+        UserConvert a = new UserConvert();
+        List<User> list = new ArrayList<>();
+        list.add(new User(1, "Igor", "Tero"));
+        list.add(new User(2, "Vasya", "Poru"));
+        HashMap<Integer, User> maper = a.process(list);
+        String expect = "Tero";
+        assertThat(maper.get(1).getCity(), is(expect));
+    }
+}
