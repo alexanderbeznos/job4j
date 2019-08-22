296ef3f7ca8e9289cbf9747179809085dac4de00
alexanderbeznos <ast1bn@mail.ru>
2019-08-23 00:10:42 +0300

    Отсортировать департаменты [#34352]

---
+++ chapter_003/src/main/java/ru/job4j/depart/Department.java
@@ -0,0 +1,83 @@
+package ru.job4j.depart;
+
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author Alexander Beznos (ast1bn@mail.ru).
+ * @version 1.0.
+ * @since 21.08.2019.
+ */
+public class Department {
+
+    /**
+     * метод добавляет верхние подразделнений и сортирует по возростанию.
+     */
+    public Set<String> structure(List<String> list) {
+        Set<String> res = new TreeSet<>(list);
+        for (String pos : list) {
+            List<String> help = Arrays.asList(pos.split("\\\\"));
+            for (int i = help.size() - 1; i >= 1; i--) {
+                List<String> now = help.subList(0, i);
+                String joined = String.join("\\", now);
+                res.add(joined);
+            }
+        }
+        return res;
+    }
+
+    /**
+     * метод сортирует по убыванию.
+     */
+    public List<String> sortlow(List<String> list) {
+        ArrayList<String> resul = new ArrayList<>();
+        ArrayList<String> step;
+        TreeSet<String> tree = setmake(list);
+        for (String o : tree) {
+            step = allset(o, list);
+            resul.addAll(step);
+        }
+        return resul;
+    }
+
+    /**
+     * метод возвращает список всех главных подразделений отсортированный по убыванию(“K2”, “K1”).
+     */
+    public TreeSet<String> setmake(List<String> list) {
+        TreeSet<String> li = new TreeSet<>(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o2.compareTo(o1);
+            }
+        });
+        for (String s : list) {
+            Pattern p = Pattern.compile("^[a-zA-Z]*[0-9]*");
+            Matcher m = p.matcher(s);
+            while (m.find()) {
+                li.add(m.group());
+            }
+
+        }
+        return li;
+    }
+
+    /**
+     * Метод возвращает лист всех отделов относящихся к одному подразделения. Отсортированы по требованию задачи.
+     */
+    public ArrayList<String> allset(String tree, List<String> list) {
+        ArrayList<String> res = new ArrayList<>();
+        for (String s : list) {
+            if (s.startsWith(tree)) {
+                res.add(s);
+            }
+        }
+        res.sort(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.length() == o2.length() ? o2.compareTo(o1) : Integer.compare(o1.length(), o2.length());
+            }
+        });
+        return res;
+    }
+}
---
+++ chapter_003/src/test/java/ru/job4j/collections/depart/DepartmentTest.java
@@ -0,0 +1,60 @@
+package ru.job4j.collections.depart;
+
+import org.junit.Test;
+import ru.job4j.depart.Department;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Alexander Beznos (ast1bn@mail.ru).
+ * @version 1.0.
+ * @since 21.08.2019.
+ */
+public class DepartmentTest {
+
+    /**
+     * тест на добавление верхних подразделнений и сортировки по возростанию.
+     */
+    @Test
+    public void whenFind() {
+        Department obj = new Department();
+        List<String> list = new ArrayList<>();
+        list.add("K1\\SK1\\SSK1");
+        list.add("K1\\SK1\\SSK2");
+        Set<String> li =  obj.structure(list);
+        List<String> re = new ArrayList<>(li);
+        String result = re.get(1);
+        String expect = "K1\\SK1";
+        assertThat(result, is(expect));
+    }
+
+    /**
+     * тест на сортировку по убываанию.
+     */
+    @Test
+    public void whenLow() {
+        Department obj = new Department();
+        List<String> list = new ArrayList<>();
+        list.add("K1\\SK1");
+        list.add("K1\\SK2");
+        list.add("K1\\SK1\\SSK1");
+        list.add("K1\\SK1\\SSK2");
+        list.add("K2");
+        list.add("K2\\SK1\\SSK1");
+        list.add("K2\\SK1\\SSK2");
+        Set<String> li =  obj.structure(list);
+        List<String> re = new ArrayList<>(li);
+        List<String> cons = obj.sortlow(re);
+        String result = cons.get(2);
+        String expect = "K2\\SK1\\SSK2";
+        assertThat(result, is(expect));
+        for (String o : cons) {
+            System.out.println(o);
+        }
+
+
+    }
+}
