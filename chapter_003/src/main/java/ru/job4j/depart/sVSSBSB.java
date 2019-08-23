7359ff78c6d23b18736137c1822e23dd371d10ff
alexanderbeznos <ast1bn@mail.ru>
2019-08-23 22:16:48 +0300

    исправлеия

---
+++ chapter_003/src/main/java/ru/job4j/depart/sVSSBSB.java
@@ -0,0 +1,230 @@
+package ru.job4j.depart;
+
+import javafx.print.Collation;
+
+import java.text.CollationElementIterator;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class sVSSBSB {
+    public static void main(String[] args) {
+
+
+
+        List<String> list = new ArrayList<>();
+//        String a = "K1";
+//        String b = "K2";
+//
+//        String eee = "K1\\SK1\\SSK1";
+//
+//        System.out.println(eee.startsWith(b));
+//
+//
+        list.add("K1\\SK1");
+        list.add("K1\\SK2");
+        list.add("K1\\SK1\\SSK1");
+        list.add("K1\\SK1\\SSK2");
+        list.add("K2");
+        list.add("K2\\SK1\\SSK1");
+        list.add("K2\\SK1\\SSK2");
+
+        list.add("K1");
+        list.add("K2\\SK1");
+
+
+        list.sort(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o2.compareTo(o1);
+            }
+        });
+//
+//
+//       ArrayList<String> qwe = new ArrayList<>();
+//
+//        TreeSet<String> li = new TreeSet<>();
+//        li.add("K1");
+//        li.add("K2");
+//
+//
+//
+//        ArrayList<String> res = new ArrayList<>();
+//        for (String out : li) {
+//            for (String s : list) {
+//                if (s.startsWith(out)) {
+//                    res.add(s);
+//                }
+//            }
+//        }
+
+
+
+
+//        list.sort(new Comparator<String>() {
+//            @Override
+//            public int compare(String o1, String o2) {
+//                return o1.length() == o2.length() ? o2.compareTo(o1) : Integer.compare(o1.length(), o2.length());
+//            }
+//        });
+
+
+//            for (String inn : list) {
+//                if (inn.contains(a)) {
+//                    qwe.add(inn);
+//                }
+//            }
+        for (String o : list) {
+            System.out.println(o);
+        }
+
+
+
+
+
+
+
+//        for (String o : qwe) {
+//            System.out.println(o);
+//        }
+
+
+
+
+//        list.add("K1\\SK1");
+//        list.add("K1\\SK2");
+//        list.add("K1\\SK1\\SSK1");
+//        list.add("K1\\SK1\\SSK2");
+//        list.add("K2");
+//        list.add("K2\\SK1\\SSK1");
+//        list.add("K2\\SK1\\SSK2");
+
+//        list.add("K1");
+//        list.add("K1\\SK1");
+//        list.add("K1\\SK2");
+//        list.add("K1\\SK1\\SSK1");
+//        list.add("K1\\SK1\\SSK2");
+//
+//
+//
+//
+//        //Collections.sort(list, new Nme());
+//        list.sort(new Comparator<String>() {
+//            @Override
+//            public int compare(String o1, String o2) {
+//                return o2.compareTo(o1);
+//            }
+//        });
+//
+//        for (String o : list) {
+//            System.out.println(o);
+//        }
+//
+//        class Nme implements Comparator<String> {
+//            @Override
+//            public int compare(String o1, String o2) {
+//                return o2.compareTo(o1);
+//            }
+//        }
+
+
+
+
+
+//        String s = "K1\\SK1\\SSK1";
+//        String q = s.replace("\\d", "");
+//        System.out.println(q);
+
+//        String[] mas = s.split("\\\\");
+//        for (int i = 0; i < mas.length; i++) {
+//            System.out.println(mas[i]);
+//        }
+//        List<String> list = new ArrayList<>();
+
+
+
+
+
+//        TreeSet<String> li = new TreeSet<>(new Comparator<String>() {
+//            @Override
+//            public int compare(String o1, String o2) {
+//                return o2.compareTo(o1);
+//            }
+//        });
+//        for (String s : list) {
+//            Pattern p = Pattern.compile("^[a-zA-Z]*[0-9]*");
+//            Matcher m = p.matcher(s);
+//            while (m.find()) {
+//                li.add(m.group());
+//            }
+//        }
+//        for (String o : li) {
+//            System.out.println(o);
+//        }
+
+
+
+
+
+
+//        li.add("K1");
+//        li.add("K1\\SK1");
+//        li.add("K1\\SK2");
+//        li.add("K1\\SK1\\SSK1");
+//        li.add("K1\\SK1\\SSK2");
+
+
+
+
+
+
+//        Pattern p = Pattern.compile("^[a-zA-Z]*[0-9]*");
+//        Matcher m = p.matcher("KL11\\SK1\\SSK1");
+//        while (m.find()) {
+//            list.add(m.group());
+//        }
+//        for (int i = 0; i < list.size(); i++) {
+//            System.out.println(list.get(i));
+//        }
+//        String s = "KL11\\SK1\\SSK1";
+//        String[] mas = s.split("^[a-zA-Z]*[0-9]*");
+//        for (int i = 0; i < mas.length; i++) {
+//            System.out.println(mas[i]);
+//        }
+
+
+
+//        List<String> list = new ArrayList<>();
+//
+//        list.add("K1\\SK1");
+//        list.add("K1\\SK2");
+//        list.add("K1\\SK1\\SSK1");
+//        list.add("K1\\SK1\\SSK2");
+//        list.add("K2");
+//        list.add("K2\\SK1\\SSK1");
+//        list.add("K2\\SK1\\SSK2");
+
+//        String joined = String.join("??", list); // «foo;bar;baz"
+//        System.out.println(joined);
+//        //List<String> a = list.subList(0, 1);
+//
+//        System.out.println(list.contains("Бобик"));
+//
+//
+//        for (int i = 0; i < a.size(); i++) {
+//            System.out.println(a.get(i));
+//        }
+
+//    }
+//    public ArrayList<String> setmake(String treeSet, List<String> list) {
+//        ArrayList<String> res = new ArrayList<>();
+//
+//        for (String inn : list) {
+//            if (inn.contains(treeSet)) {
+//                res.add(inn);
+//            }
+//        }
+//
+//        return res;
+    }
+}
