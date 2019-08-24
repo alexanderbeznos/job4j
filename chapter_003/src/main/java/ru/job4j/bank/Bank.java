3dabe1886fc570c8440913e5e0a131c93016f042
alexanderbeznos <ast1bn@mail.ru>
2019-08-24 20:49:40 +0300

    6. Тестовое задание из модуля коллекции Lite переделать на Stream API.[#127814]

--- chapter_003/src/main/java/ru/job4j/bank/Bank.java
+++ chapter_003/src/main/java/ru/job4j/bank/Bank.java
@@ -1,6 +1,7 @@
 package ru.job4j.bank;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * @author Alexander Beznos (ast1bn@mail.ru).
@@ -28,45 +29,63 @@ public class Bank {
      * добавить счёт пользователю.
      */
     public void addAccountToUser(String passport, Account account) {
-        for (Map.Entry<User, List<Account>> user : map.entrySet()) {
-            if (user.getKey().getPassport().equals(passport)) {
-                user.getValue().add(account);
-            }
-        }
+//        for (Map.Entry<User, List<Account>> user : map.entrySet()) {
+//            if (user.getKey().getPassport().equals(passport)) {
+//                user.getValue().add(account);
+//            }
+//        }
+        Set<User> list = map.keySet();
+        List<User> a = list.stream().filter(p -> p.getPassport().
+                equals(passport)).collect(Collectors.toList());
+        User user = a.get(0);
+        map.get(user).add(account);
     }
 
     /**
      * удалить один счёт пользователя.
      */
     public void deleteAccountFromUser(String passport, Account account) {
-        List<Account> list = findAccountsOnPassport(passport);
-        Iterator<Account> iter = list.iterator();
-        while (iter.hasNext()) {
-            Account com = iter.next();
-            if (com.getRequisites().equals(account.getRequisites())) {
-                list.remove(com);
-            }
-        }
+//        List<Account> list = findAccountsOnPassport(passport);
+//        Iterator<Account> iter = list.iterator();
+//        while (iter.hasNext()) {
+//            Account com = iter.next();
+//            if (com.getRequisites().equals(account.getRequisites())) {
+//                list.remove(com);
+//            }
+//        }
+        Set<User> list = map.keySet();
+        List<User> a = list.stream().filter(p -> p.getPassport().
+                equals(passport)).collect(Collectors.toList());
+        User user = a.get(0);
+        map.get(user).remove(account);
+
+
     }
 
     /**
      * получить список счетов для пользователя.
      */
     public List<Account> getUserAccounts(String passport) {
-        List<Account> ret = new ArrayList<>();
-        for (Map.Entry<User, List<Account>> user : map.entrySet()) {
-            if (user.getKey().getPassport().equals(passport)) {
-                ret.addAll(user.getValue());
-            }
-        }
-        return ret;
+//        List<Account> ret = new ArrayList<>();
+//        for (Map.Entry<User, List<Account>> user : map.entrySet()) {
+//            if (user.getKey().getPassport().equals(passport)) {
+//                ret.addAll(user.getValue());
+//            }
+//        }
+//        return ret;
+        Set<User> list = map.keySet();
+        List<User> a = list.stream().filter(p -> p.getPassport().
+                equals(passport)).collect(Collectors.toList());
+        User user = a.get(0);
+        List<Account> fin = map.get(user);
+        return fin;
     }
 
     /**
      * метод для перечисления денег с одного счёта на другой счёт.
      */
     public boolean transferMoney(String srcPassport, String srcRequisite,
-                                  String destPassport, String dstRequisite, double amount) {
+                                 String destPassport, String dstRequisite, double amount) {
         Account from = findAccount(findAccountsOnPassport(srcPassport), srcRequisite);
         Account in = findAccount(findAccountsOnPassport(destPassport), dstRequisite);
         return from != null && in != null && from.transfer(in, amount);
@@ -76,26 +95,36 @@ public class Bank {
      * метод по поиску аккаунтов по паспорту.
      */
     public List<Account> findAccountsOnPassport(String passport) {
-        List<Account> a = new ArrayList<>();
-        for (Map.Entry<User, List<Account>> user : map.entrySet()) {
-            if (user.getKey().getPassport().equals(passport)) {
-                a = user.getValue();
-            }
-        }
-        return a;
+//        List<Account> a = new ArrayList<>();
+//        for (Map.Entry<User, List<Account>> user : map.entrySet()) {
+//            if (user.getKey().getPassport().equals(passport)) {
+//                a = user.getValue();
+//            }
+//        }
+//        return a;
+        Set<User> list = map.keySet();
+        List<User> a = list.stream().filter(p -> p.getPassport().
+                equals(passport)).collect(Collectors.toList());
+        User user = a.get(0);
+        List<Account> fin = map.get(user);
+        return fin;
+
     }
 
     /**
      * метод по поиску Account из списка Account.
      */
     public Account findAccount(List<Account> list, String requisite) {
-        Account a = null;
-        for (Account user : list) {
-            if (user.getRequisites().equals(requisite)) {
-                a = user;
-            }
-        }
-        return a;
+//        Account a = null;
+//        for (Account user : list) {
+//            if (user.getRequisites().equals(requisite)) {
+//                a = user;
+//            }
+//        }
+//        return a;
+        List<Account> a = list.stream().filter(p -> p.getRequisites().
+                equals(requisite)).collect(Collectors.toList());
+        Account ac = a.get(0);
+        return ac;
     }
-
 }
