c89aaf30f190ca4337eda932c9e7403ab2a368ee
alexanderbeznos <ast1bn@mail.ru>
2019-08-30 18:26:13 +0300

    1. Читаем файл конфигурации[#174216] v2

--- chapter_006/src/test/java/ru/job4j/inout/ConfigTest.java
+++ chapter_006/src/test/java/ru/job4j/inout/ConfigTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertThat;
 public class ConfigTest {
     @Test
     public void whenMapWithDate() {
-        Config file = new Config("C:\\projects\\job4j\\app.properties");
+        Config file = new Config("./../app.properties");
         file.load();
         assertThat(file.value("hibernate.dialect"), is("org.hibernate.dialect.PostgreSQLDialect"));
         assertThat(file.value("hibernate.connection.url"), is("jdbc:postgresql://127.0.0.1:5432/trackstudio"));
