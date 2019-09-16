537add42eabdb6f95c830f21cba009c84858bd88
alexanderbeznos <ast1bn@mail.ru>
2019-09-16 18:09:47 +0300

    1. Реализовать коллекцию Set на массиве[#174207] v3

--- chapter_005/src/main/java/ru/job4j/set/SimpleSet.java
+++ chapter_005/src/main/java/ru/job4j/set/SimpleSet.java
@@ -3,6 +3,7 @@ package ru.job4j.set;
 import ru.job4j.list.DinList;
 
 import java.util.Iterator;
+import java.util.Objects;
 
 public class SimpleSet<E> implements Iterable<E> {
     private DinList dinList = new DinList();
@@ -16,16 +17,9 @@ public class SimpleSet<E> implements Iterable<E> {
     public boolean contains(E e) {
         boolean result = false;
         for (int i = 0; i < dinList.size(); i++) {
-            if (e == null) {
-                if (dinList.get(i) == e) {
-                    result = true;
-                    break;
-                }
-            } else {
-                if (e.equals(dinList.get(i))) {
-                    result = true;
-                    break;
-                }
+            if (Objects.equals(dinList.get(i), e)) {
+                result = true;
+                break;
             }
         }
         return result;
