c3aebc3e93310aeffc963ae99591acb865003ab1
alexanderbeznos <ast1bn@mail.ru>
2019-09-16 17:15:23 +0300

    1. Реализовать коллекцию Set на массиве[#174207] v2

--- chapter_005/src/main/java/ru/job4j/set/SimpleSet.java
+++ chapter_005/src/main/java/ru/job4j/set/SimpleSet.java
@@ -8,7 +8,7 @@ public class SimpleSet<E> implements Iterable<E> {
     private DinList dinList = new DinList();
 
     public void add(E value) {
-        if (!contains(value) && value != null) {
+        if (!contains(value)) {
             dinList.add(value);
         }
     }
@@ -16,8 +16,16 @@ public class SimpleSet<E> implements Iterable<E> {
     public boolean contains(E e) {
         boolean result = false;
         for (int i = 0; i < dinList.size(); i++) {
-            if (dinList.get(i).equals(e)) {
-                result = true;
+            if (e == null) {
+                if (dinList.get(i) == e) {
+                    result = true;
+                    break;
+                }
+            } else {
+                if (e.equals(dinList.get(i))) {
+                    result = true;
+                    break;
+                }
             }
         }
         return result;
--- chapter_005/src/test/java/ru/job4j/set/SimpleSetTest.java
+++ chapter_005/src/test/java/ru/job4j/set/SimpleSetTest.java
@@ -10,18 +10,13 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 public class SimpleSetTest {
-    private SimpleSet<Integer> simpleSet;
-
-    @Before
-    public void beforeTest() {
-        simpleSet = new SimpleSet<>();
-        simpleSet.add(1);
-        simpleSet.add(2);
-        simpleSet.add(3);
-    }
+    private SimpleSet<Integer> simpleSet = new SimpleSet<>();
 
     @Test
     public void whenAddDuplicateElementsThenSizeDoesNotChange() {
+        simpleSet.add(1);
+        simpleSet.add(2);
+        simpleSet.add(3);
         assertThat(simpleSet.size(), is(3));
         simpleSet.add(3);
         assertThat(simpleSet.size(), is(3));
@@ -32,6 +27,9 @@ public class SimpleSetTest {
 
     @Test
     public void hasNextNextSequentialInvocation() {
+        simpleSet.add(1);
+        simpleSet.add(2);
+        simpleSet.add(3);
         Iterator<Integer> it = simpleSet.iterator();
         assertThat(it.hasNext(), is(true));
         assertThat(it.next(), is(1));
@@ -41,5 +39,18 @@ public class SimpleSetTest {
         assertThat(it.next(), is(3));
         assertThat(it.hasNext(), is(false));
 
+    }
+
+    @Test
+    public void hasAdd() {
+        simpleSet.add(1);
+        simpleSet.add(null);
+        simpleSet.add(2);
+        simpleSet.add(1);
+        Iterator<Integer> it = simpleSet.iterator();
+        assertThat(it.next(), is(1));
+        assertThat(it.next() == null, is(true));
+        assertThat(it.next(), is(2));
+        assertThat(it.hasNext(), is(false));
     }
 }
