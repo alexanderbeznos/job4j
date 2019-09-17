42f4134a1fef7b48c503d70d2f30db1f18b8ac7c
alexanderbeznos <ast1bn@mail.ru>
2019-09-17 15:17:55 +0300

    8. Реализовать собственную структуру данных - HashMap[#174194]

---
+++ chapter_005/src/main/java/ru/job4j/map/MyHashMap.java
@@ -0,0 +1,143 @@
+package ru.job4j.map;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+
+/**
+ * @author Alexander Beznos (ast1bn@mail.ru).
+ * @version 1.0
+ * @since 17.09.2019.
+ */
+public class MyHashMap<K, V> implements Iterable{
+    private Node<K, V>[] container;
+    private int size = 0;
+    private int modCount = 0;
+
+    public MyHashMap() {
+        container = new Node[16];
+    }
+
+    private int hash(Object key) {
+        int h = 0;
+        if (key == null) {
+            return h;
+        } else {
+            h = key.hashCode();
+            h = h ^ (h >>> 16);
+        }
+        return h;
+    }
+
+    private int indexFor(int hash) {
+        return hash & (container.length - 1);
+    }
+
+
+    public boolean insert(K key, V value) {
+        boolean result = false;
+        if (size == container.length) {
+            increase();
+        }
+        int hash = hash(key);
+        int index = indexFor(hash);
+        Node<K, V> old = container[index];
+        if (old == null) {
+            container[index] = new Node<>(key, value);
+            result = true;
+            modCount++;
+            size++;
+        }
+        return result;
+    }
+
+    public V get(K key) {
+        int hash = hash(key);
+        int index = indexFor(hash);
+        Node<K, V> node = container[index];
+        V value = null;
+        if (node != null) {
+            value = node.value;
+
+        }
+        return value;
+    }
+
+    public boolean delete(K key) {
+        boolean result = false;
+        if (container.length > 0) {
+            int hash = hash(key);
+            int index = indexFor(hash);
+            Node<K, V> node = container[index];
+            if (node != null && key.equals(node.key)) {
+                container[index] = null;
+                result = true;
+                modCount++;
+                size--;
+            }
+        }
+        return result;
+    }
+
+    public void increase() {
+        Node<K, V>[] old = container;
+        container = new Node[old.length * 2];
+        System.arraycopy(old, 0, container, 0, old.length - 1);
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    private class Node<K, V> {
+        private final K key;
+        private V value;
+
+        public Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+        return new Iterator<>() {
+            private int expectedModCount = modCount;
+            private int count;
+            private int current;
+
+            @Override
+            public boolean hasNext() {
+                return size > this.count;
+            }
+
+            @Override
+            public K next() {
+
+                if (expectedModCount != size) {
+                    throw new ConcurrentModificationException();
+                }
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                for (int i = current; i < container.length; i++) {
+                    if (container[i] != null) {
+                        count++;
+                        current = i + 1;
+                        return container[i].getKey();
+                    }
+                }
+                return null;
+            }
+        };
+    }
+}
---
+++ chapter_005/src/test/java/ru/job4j/map/MyHashMapTest.java
@@ -0,0 +1,64 @@
+package ru.job4j.map;
+
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
+import java.util.Iterator;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Alexander Beznos (ast1bn@mail.ru).
+ * @version 1.0
+ * @since 17.09.2019.
+ */
+public class MyHashMapTest {
+    private MyHashMap<String, Integer> map;
+
+    @Before
+    public void beforeTest() {
+        map = new MyHashMap<>();
+        map.insert("1", 1);
+        map.insert("2", 2);
+        map.insert("3", 3);
+        map.insert("4", 4);
+        map.insert("5", 5);
+        map.insert("6", 6);
+    }
+
+    @Test
+    public void whenInsert() {
+        assertThat(map.getSize(), is(6));
+
+    }
+
+    @Test
+    public void whenGetAndInsert() {
+        assertThat(map.get("6"), is(6));
+        assertThat(map.insert("6", 6), is(false));
+    }
+
+    @Test
+    public void whenDelete() {
+        assertThat(map.delete("6"), is(true));
+        assertThat(map.insert("6", 6), is(true));
+    }
+
+    @Test
+    public void hasNext() {
+        Iterator<String> it = map.iterator();
+        assertThat(it.hasNext(), Matchers.is(true));
+        assertThat(it.next(), Matchers.is("1"));
+        assertThat(it.hasNext(), Matchers.is(true));
+        assertThat(it.next(), Matchers.is("2"));
+        assertThat(it.hasNext(), Matchers.is(true));
+        assertThat(it.next(), Matchers.is("3"));
+        assertThat(it.hasNext(), Matchers.is(true));
+        assertThat(it.next(), Matchers.is("4"));
+        assertThat(it.hasNext(), Matchers.is(true));
+        assertThat(it.next(), Matchers.is("5"));
+        assertThat(it.hasNext(), Matchers.is(true));
+        assertThat(it.next(), Matchers.is("6"));
+        assertThat(it.hasNext(), Matchers.is(false));
+    }
+}
